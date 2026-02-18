94072a422af4757023164f8fa3131f93c6843549
sonygatlaa <134613431+sonyvinny77@users.noreply.github.com>
2026-02-18 13:58:47 +0530

    Create para.groovy

---
+++ vars/para.groovy
@@ -0,0 +1,62 @@
+def call(String envName) {
+
+    pipeline {
+        agent any
+
+        stages {
+
+            stage('Download Code') {
+                steps {
+                    git 'https://github.com/IntelliqDevops/maven.git'
+                }
+            }
+
+            stage('Build Maven Project') {
+                steps {
+                    sh 'mvn clean package'
+                }
+            }
+
+            stage('Prepare Docker Context') {
+                steps {
+                    sh '''
+                        rm -rf docker
+                        mkdir docker
+                        cp webapp/target/webapp.war docker/webapp.war
+                        cp Dockerfile docker/
+                    '''
+                }
+            }
+
+            stage('Build Docker Image') {
+                steps {
+                    script {
+                        if (envName == "dev") {
+                            sh 'docker build -t regapp:dev docker'
+                        } else if (envName == "prod") {
+                            sh 'docker build -t regapp:prod docker'
+                        }
+                    }
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    script {
+                        if (envName == "dev") {
+                            sh '''
+                                docker rm -f regapp-dev || true
+                                docker run -d -p 8081:8080 --name regapp-dev regapp:dev
+                            '''
+                        } else if (envName == "prod") {
+                            sh '''
+                                docker rm -f regapp-prod || true
+                                docker run -d -p 8082:8080 --name regapp-prod regapp:prod
+                            '''
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
